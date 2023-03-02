6ecad4800dc06d1827ca1a8207d0d460e3210a00
Olexandr Beisiuk <57046480+Bolexandr@users.noreply.github.com>
2023-03-02 16:24:55 +0200

    Аdd photo

--- src/dataBase/Susi.js
+++ src/dataBase/Susi.js
@@ -3,84 +3,96 @@ const susis = [
     id: 1,
     name: "Філадельфія з огірком",
     price: 150,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A4%D1%96%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D1%96%D1%8F-%D0%B7-%D0%BE%D0%B3%D1%96%D1%80%D0%BA%D0%BE%D0%BC-826x0-min.png?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 2,
     name: "Сяке футомакі",
     price: 130,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A1%D1%8F%D0%BA%D0%B5-%D1%84%D1%83%D1%82%D1%83%D0%BC%D0%B0%D0%BA%D1%96-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 3,
     name: "Унагі футумакі",
     price: 150,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A3%D0%BD%D0%B0%D0%B3%D1%96-%D1%84%D1%83%D1%82%D1%83%D0%BC%D0%B0%D0%BA%D1%96-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 4,
     name: "ТайгерРол",
     price: 195,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A2%D0%B0%D0%B9%D0%B3%D0%B5%D1%80-%D1%80%D0%BE%D0%BB-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 5,
     name: "Канада",
     price: 185,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%9A%D0%B0%D0%BD%D0%B0%D0%B4%D0%B0-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 6,
     name: "Чіз рол",
     price: 130,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A7%D1%96%D0%B7-%D1%80%D0%BE%D0%BB-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 7,
     name: "4 сири",
     price: 130,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/4-%D1%81%D0%B8%D1%80%D0%B8-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 8,
     name: "Рінго",
     price: 130,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A0%D1%96%D0%BD%D0%B3%D0%BE-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 9,
     name: "Делішеі",
     price: 155,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%94%D0%B5%D0%BB%D1%96%D1%88%D0%B5%D1%81-min.jpg?raw=trues",
     ingredients: ["", ""],
   },
   {
     id: 10,
     name: "Дракон Лео",
     price: 150,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%94%D1%80%D0%B0%D0%BA%D0%BE%D0%BD-%D0%BB%D0%B5%D0%BE-min.png?raw=trues",
     ingredients: ["", ""],
   },
   {
     id: 11,
     name: "Філадельфія з авокадо",
     price: 160,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A4%D1%96%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D1%96%D1%8F-%D0%B7-%D0%BE%D0%B3%D1%96%D1%80%D0%BA%D0%BE%D0%BC-826x0-min.png?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 12,
     name: "Печена філадельфія",
     price: 185,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%9F%D0%B5%D1%87%D0%B5%D0%BD%D0%B0-%D1%84%D1%96%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D1%96%D1%8F-min.jpeg?raw=true",
     ingredients: ["", ""],
   },
   {
@@ -94,42 +106,48 @@ const susis = [
     id: 14,
     name: "Тобіко з лососем",
     price: 140,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A2%D0%BE%D0%B1%D1%96%D0%BA%D0%BE-%D0%B7-%D0%BB%D0%BE%D1%81%D0%BE%D1%81%D0%B5%D0%BC-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 15,
     name: "Ікура Каліфорнія",
     price: 160,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%86%D0%BA%D1%83%D1%80%D0%B0-%D0%BA%D0%B0%D0%BB%D1%96%D1%84%D0%BE%D1%80%D0%BD%D1%96%D1%8Fi-min.png?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 16,
     name: "Гаряча філадельфія",
     price: 160,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%93%D0%B0%D1%80%D1%8F%D1%87%D0%B0-%D0%A4%D1%96%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D1%96%D1%8F-min.jpeg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 17,
     name: "Гарячий ебі",
     price: 165,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%93%D0%B0%D1%80%D1%8F%D1%87%D0%B8%D0%B9-%D0%B5%D0%B1%D1%96-min.jpeg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 18,
     name: "Гарячий дракон",
     price: 180,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%93%D0%B0%D1%80%D1%8F%D1%87%D0%B8%D0%B9-%D0%94%D1%80%D0%B0%D0%BA%D0%BE%D0%BD-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
     id: 19,
     name: "Хіросіма",
     price: 180,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%A5%D1%96%D1%80%D0%BE%D1%81%D1%96%D0%BC%D0%B0-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
@@ -143,7 +161,8 @@ const susis = [
     id: 21,
     name: "Колорадо",
     price: 180,
-    photo: "./dataBase/images",
+    photo:
+      "https://github.com/Bolexandr/SuShi/blob/main/src/components/images/%D0%9A%D0%BE%D0%BB%D0%BE%D1%80%D0%B0%D0%B4%D0%BE-min.jpg?raw=true",
     ingredients: ["", ""],
   },
   {
